41100fbb27e34512a6ebb94e5fc790ff44789462
kazuki morita <kz.morit@gmail.com>
2018-08-31 14:11:51 +0900

    first commit

---
+++ Classes/TitleScene.cpp
@@ -0,0 +1,8 @@
+//
+//  TitleScene.cpp
+//  ActionSample
+//
+//  Created by 森田和樹 on 2018/08/31.
+//
+
+#include "TitleScene.h"
---
+++ Classes/TitleScene.hpp
@@ -0,0 +1,13 @@
+//
+//  TitleScene.hpp
+//  ActionSample
+//
+//  Created by 森田和樹 on 2018/08/31.
+//
+
+#ifndef TitleScene_hpp
+#define TitleScene_hpp
+
+#include <stdio.h>
+
+#endif /* TitleScene_hpp */
