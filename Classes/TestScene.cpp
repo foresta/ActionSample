7f8fbd35935aac4e543ffb5f7eb333068783d77a
kazuki morita <kz.morit@gmail.com>
2018-09-02 14:21:24 +0900

    add sumple actions

--- Classes/TestScene.cpp
+++ Classes/TestScene.cpp
@@ -12,7 +12,7 @@ bool TestScene::init()
     this->setupIcon();
     this->setupResetButton();
     
-    this->action = [this](){ this->move(); };
+    this->action = CC_CALLBACK_0(TestScene::targeted_action, this);
     
     return true;
 }
@@ -67,60 +67,69 @@ void TestScene::onEnterTransitionDidFinish()
 #pragma mark -
 #pragma mark private
 
+#pragma mark Besic
+
 void TestScene::move()
 {
-    this->icon->runAction(MoveBy::create(3.f, Vec2(50.f, 0)));
+    this->icon->runAction(MoveBy::create(1.f, Vec2(50.f, 0)));
 }
 
 void TestScene::rotate()
 {
-    
+    this->icon->runAction(RotateBy::create(1.f, 360.f));
 }
 
 void TestScene::scale()
 {
-    
+    this->icon->runAction(ScaleBy::create(1.f, 2.f));
 }
 
 void TestScene::skew()
 {
-    
+    this->icon->runAction(SkewBy::create(1.f, 30.f,0.f));
 }
 
 void TestScene::resize()
 {
-    
+    this->icon->runAction(ResizeBy::create(1.f, Size(100,50)));
 }
 
 void TestScene::jump()
 {
-    
+    float jumpHeight { 30.f };
+    int count { 5 };
+    this->icon->runAction(JumpBy::create(1.f, Vec2::ZERO, jumpHeight, count));
 }
 
 void TestScene::bezier()
 {
+    ccBezierConfig config {};
+    config.endPosition = Vec2(50,0);
+    config.controlPoint_1 = Vec2::ZERO;
+    config.controlPoint_2 = Vec2(25,50);
     
+    this->icon->runAction(BezierBy::create(1.f, config));
 }
 
 void TestScene::blink()
 {
-    
+    this->icon->runAction(Blink::create(2.f, 5));
 }
 
 void TestScene::fade()
 {
-    
+    this->icon->setOpacity(0.f);
+    this->icon->runAction(FadeTo::create(2.f, 255));
 }
 
 void TestScene::tint()
 {
-    
+    this->icon->runAction(TintTo::create(1.f, Color3B::RED));
 }
 
-void TestScene::delay()
-{
-    
-}
+#pragma mark -
+
+
 
 void TestScene::action_float()
 {
@@ -132,14 +141,42 @@ void TestScene::animate()
     
 }
 
+void TestScene::sequence()
+{
+    this->icon->runAction(
+        Sequence::create(
+            MoveBy::create(.5f, Vec2(50, 0)),
+            MoveBy::create(1.f, Vec2(-100, 0)),
+            MoveBy::create(.5f, Vec2(50, 0)),
+            nullptr));
+}
+
+void TestScene::spawn()
+{
+    this->icon->runAction(
+        Spawn::create(
+            MoveBy::create(1.f, Vec2(50,0)),
+            ScaleBy::create(1.f, 1.5f),
+            RotateBy::create(1.f, 360),
+            nullptr)
+    );
+}
+
+void TestScene::delay()
+{
+    this->icon->runAction(Sequence::createWithTwoActions(DelayTime::create(3.f), MoveBy::create(1.f, Vec2(50, 0))));
+}
+
 void TestScene::reverse_time()
 {
-    
+    MoveBy* move { MoveBy::create(1.f, Vec2(50,0)) };
+    ReverseTime* reverse { ReverseTime::create(move->clone()) };
+    this->icon->runAction(Sequence::createWithTwoActions(move, reverse));
 }
 
 void TestScene::targeted_action()
 {
-    
+    this->runAction(TargetedAction::create(this->icon, MoveBy::create(1.f, Vec2(50, 0))));
 }
 
 void TestScene::repeat()
@@ -152,15 +189,6 @@ void TestScene::repeat_forever()
     
 }
 
-void TestScene::sequence()
-{
-    
-}
-
-void TestScene::spawn()
-{
-    
-}
 
 
 void TestScene::show()
